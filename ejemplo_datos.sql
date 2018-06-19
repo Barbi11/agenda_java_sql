aee6cee60cfa78d94b2598338732d55d90851e21
Barbi11 <33290066+Barbi11@users.noreply.github.com>
2018-06-19 15:43:14 -0300

    Update ejemplo_datos.sql

--- ejemplo_datos.sql
+++ ejemplo_datos.sql
@@ -2,5 +2,5 @@
 
 SELECT contacto(37612321, 'Bárbara', 'Cerviño', 'Gral. Arias 2640', '2051-1496', 'barbaracervino@hotail.com');
 SELECT contacto(25969243, 'German', 'Basisty', 'Maipu 251', '2084-9468', 'basisty.german@gmail.com');
-ELECT contacto(35000000, 'Raul', 'Sanchez', NULL, NULL, NULL);
+SELECT contacto(35000000, 'Raul', 'Sanchez', NULL, NULL, NULL);
 SELECT contacto(16555666, 'Nilda', 'Martínez', NULL, NULL, NULL);
